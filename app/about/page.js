74813ef0102908f765c387916692491d2661723d
philkoerber <philipp.koerber.93@gmail.com>
2023-09-14 18:51:23 +0900

    add content to about

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 function About(props) {
-  return <div className="">hi</div>;
+  return (
+    <div className="h-[90vh] flex justify-center items-center">
+      <div className="max-w-[600px] flex flex-col gap-2">
+        <p>
+          Hello, my name is Sasha, and on this channel I post videos about how I
+          make music. I'm a long time play as a DJ and producer for experimental
+          music (mostly hip-hop based) .
+        </p>
+        <p>
+          You can check my releases on bandcamp. I also work as a composer for
+          films, animation and video games, as well as producing various musical
+          groups. For 10 past years I made music for: Netflix, HBO, Adidas,
+          Coca-Cola, Dior, Yves Saint Laurent, Mercedes-Benz and more ...
+        </p>
+        <p>
+          Music producer and artist from : DZA // ACD // how2make rec. // RBMA
+          london // mumiy troll // Nana the Shrimp
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
