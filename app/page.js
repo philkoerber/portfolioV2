f6fd1e9e9da6cae731d3fdcd38f16540d83c803b
philkoerber <philipp.koerber.93@gmail.com>
2025-04-21 12:44:17 +0200

    organize size

--- app/page.js
+++ app/page.js
@@ -6,17 +6,22 @@ export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center text-4xl p-24">
       <Zapper>
-        <Link href={"/about"} className="">
-          <div className="relative">
-            <img src="./pk.svg" className="invert sepia opacity-95" />
+        <Link href="/about" className="relative group">
+          <div className="relative w-[60vw] max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg">
+            <img
+              src="./pk.svg"
+              alt="Logo"
+              className="w-full h-auto invert sepia opacity-95"
+            />
             <FaHandPointer
-              className="right-0 bottom-0 absolute w-[6vw] h-[6vw] text-antique animate-pulse"
+              className="absolute right-0 bottom-0 w-[10vw] max-w-[60px] text-antique animate-pulse group-hover:scale-110 transition-transform"
               style={{
                 animationDuration: "0.9s",
                 animationTimingFunction: "ease-in-out",
                 animationIterationCount: "infinite",
               }}
-            />          </div>
+            />
+          </div>
         </Link>
       </Zapper>
     </main>
