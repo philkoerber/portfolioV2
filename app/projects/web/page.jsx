3a35b939979319815ff4a47c274f969979594278
philkoerber <philipp.koerber.93@gmail.com>
2025-07-06 10:01:46 +0200

    update url

--- app/projects/web/page.jsx
+++ app/projects/web/page.jsx
@@ -11,7 +11,8 @@ const projects = [
       "A chatbot built with LlamaIndex that answers questions about Pokémon Red randomized speedruns by searching through exported CSV data. Integrated into a Next.js app for seamless user interaction.",
     stack: ["Next.js", "LlamaIndex"],
     github: "https://github.com/philkoerber/sswwiimm-randomizer", // Replace with actual repo if public
-    video: "https://www.youtube.com/embed/0KVYr7e5JtE"
+    video: "https://www.youtube.com/embed/0KVYr7e5JtE",
+    url: "https://sswwiimm-randomizer.vercel.app/chat",
   },
   {
     title: "Soulscript.Cards",
